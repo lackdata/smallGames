5a590c9b20a95942185b1931dc9248db5203a122
lackdata <1528549352@qq.com>
2018-01-19 09:03:39 +0800

    Update wzq.component.ts

--- src/app/wzq/wzq.component.ts
+++ src/app/wzq/wzq.component.ts
@@ -8,18 +8,12 @@ import {Component, OnInit} from '@angular/core';
 export class WzqComponent implements OnInit {
   wzqArr: any[] = [];
   inputColor = 'black';
-  game = {over: false, info: ''}
+  game = {over: false, info: ''};
   index1 = 0;
   index2 = 0;
 
   constructor() {
-    for (let i = 0; i < 16; i++) {
-      let lineArr: any[] = [];
-      for (let x = 0; x < 16; x++) {
-        lineArr.push({color: 'none', empty: false});
-      }
-      this.wzqArr.push(lineArr);
-    }
+    this.clear();
   }
 
   check(y, x) {
@@ -28,29 +22,24 @@ export class WzqComponent implements OnInit {
     }
     this.wzqArr[y][x].color = this.inputColor;
     this.wzqArr[y][x].empty = true;
-    this.checkWin(this.checkWin1(y, x), this.checkWin8(y, x))
-    this.checkWin(this.checkWin2(y, x), this.checkWin7(y, x))
-    this.checkWin(this.checkWin3(y, x), this.checkWin6(y, x))
-    this.checkWin(this.checkWin4(y, x), this.checkWin5(y, x))
+    this.checkWin(this.checkNum(y, x,'col-1','line-1'), this.checkNum(y, x,'col+1','line+1'))
+    this.checkWin(this.checkNum(y, x,'col-1','line'), this.checkNum(y, x,'col+1','line'))
+    this.checkWin(this.checkNum(y, x,'col-1','line+1'), this.checkNum(y, x,'col+1','line-1'))
+    this.checkWin(this.checkNum(y, x,'col','line-1'), this.checkNum(y, x,'col','line+1'))
     this.inputColor === 'black' ? this.inputColor = 'white' : this.inputColor = 'black';
   }
 
   checkWin(index1Fn, index2Fn) {
-    console.log(this.index1 + ',' + this.index2);
     if (this.index1 + this.index2 === 4) {
       this.game.over = true;
       this.inputColor === 'white' ? this.game.info = '白子赢了' : this.game.info = '黑子赢了';
-      return;
     }
     this.index1 = 0;
     this.index2 = 0;
   }
 
   clear() {
-    this.game = {
-      over: false,
-      info: ''
-    };
+    this.game = {over: false, info: ''};
     this.wzqArr = [];
     for (let i = 0; i < 16; i++) {
       let lineArr: any[] = [];
@@ -59,107 +48,16 @@ export class WzqComponent implements OnInit {
       }
       this.wzqArr.push(lineArr);
     }
-    this.index1 = 0;
-    this.index2 = 0;
   }
 
-  checkWin1(col, line) {
-    if (col - 1 < 0 || line - 1 < 0) {
-      return;
+  checkNum(col, line,fn1,fn2) {
+      if ( eval(fn1) < 0 || eval(fn2) < 0 || this.wzqArr[eval(fn1)][eval(fn2)].empty === false || this.wzqArr[eval(fn1)][eval(fn2)].color !== this.inputColor) {
+        return;
+      } else {
+        this.index1++;
+        this.checkNum(eval(fn1), eval(fn2),fn1,fn2);
+      }
     }
-    if (this.wzqArr[col - 1][line - 1].empty === false || this.wzqArr[col - 1][line - 1].color !== this.inputColor) {
-      return;
-    } else {
-      this.index1++;
-      this.checkWin1(col - 1, line - 1);
-    }
-  }
-
-  checkWin2(col, line) {
-    if (col - 1 < 0 || line < 0) {
-      return;
-    }
-    if (this.wzqArr[col - 1][line].empty === false || this.wzqArr[col - 1][line].color !== this.inputColor) {
-      return;
-    } else {
-      this.index1++;
-      this.checkWin2(col - 1, line);
-    }
-  }
-
-  checkWin3(col, line) {
-    if (col - 1 < 0 || line + 1 >= 16) {
-      return;
-    }
-    if (this.wzqArr[col - 1][line + 1].empty === false || this.wzqArr[col - 1][line + 1].color !== this.inputColor) {
-      return;
-    } else {
-      this.index1++;
-      this.checkWin3(col - 1, line + 1);
-    }
-  }
-
-  checkWin4(col, line) {
-    if (col < 0 || line - 1 < 0) {
-      return;
-    }
-    if (this.wzqArr[col][line - 1].empty === false || this.wzqArr[col][line - 1].color !== this.inputColor) {
-      return;
-    } else {
-      this.index1++;
-      this.checkWin4(col, line - 1);
-    }
-  }
-
-  checkWin5(col, line) {
-    if (col < 0 || line + 1 >= 16) {
-      return;
-    }
-    if (this.wzqArr[col][line + 1].empty === false || this.wzqArr[col][line + 1].color !== this.inputColor) {
-      return;
-    } else {
-      this.index2++;
-      this.checkWin5(col, line + 1);
-    }
-  }
-
-  checkWin6(col, line) {
-    if (col + 1 >= 16 || line - 1 < 0) {
-      return;
-    }
-    if (this.wzqArr[col + 1][line - 1].empty === false || this.wzqArr[col + 1][line - 1].color !== this.inputColor) {
-      return;
-    } else {
-      this.index2++;
-      this.checkWin6(col + 1, line - 1);
-    }
-  }
-
-  checkWin7(col, line) {
-    if (col + 1 >= 16 || line < 0) {
-      return;
-    }
-    if (this.wzqArr[col + 1][line].empty === false || this.wzqArr[col + 1][line].color !== this.inputColor) {
-      return;
-    } else {
-      this.index2++;
-      this.checkWin7(col + 1, line);
-    }
-  }
-
-  checkWin8(col, line) {
-    if (col + 1 >= 16 || line + 1 >= 16) {
-      return;
-    }
-    ;
-    if (this.wzqArr[col + 1][line + 1].empty === false || this.wzqArr[col + 1][line + 1].color !== this.inputColor) {
-      return;
-    } else {
-      this.index2++;
-      this.checkWin8(col + 1, line + 1);
-    }
-  }
-
 
   ngOnInit() {
   }
